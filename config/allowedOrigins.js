a5b31efca362d8b0c2954dcca195905e76e90aee
vincentmays006 <104940949+vincentmays006@users.noreply.github.com>
2024-04-28 20:53:25 -0600

    added testing site url

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,15 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://0.0.0.0/0',
-    'https://dazzling-snickerdoodle-777101.netlify.app/'
+    'https://54.84.236.175',
+    'https://44.219.53.183',
+    'https://2600:1f18:16e:df00::64',
+    'https://2600:1f18:2489:8202::c8',
 ];
 
 module.exports = allowedOrigins;
+
+// 54.84.236.175
+// 44.219.53.183
+// 2600:1f18:16e:df00::64
+// 2600:1f18:2489:8202::c8
