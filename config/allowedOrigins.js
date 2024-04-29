00a5a4a91ead27e8595c14e882446f3d0d9b1352
vincentmays006 <104940949+vincentmays006@users.noreply.github.com>
2024-04-28 21:05:26 -0600

    trying to fix cors bs

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,13 +1,4 @@
-const allowedOrigins = [
-    'https://www.yoursite.com',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
-    'http://0.0.0.0/0',
-    'https://54.84.236.175',
-    'https://44.219.53.183',
-    'https://2600:1f18:16e:df00::64',
-    'https://2600:1f18:2489:8202::c8',
-];
+const allowedOrigins = ['*'];
 
 module.exports = allowedOrigins;
 
